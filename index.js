320263db23c248dace1279c4e4b2b970c6d3b82f
Mario Gentry-Humphrey <mario.gentry89@yahoo.com>
2022-05-22 14:28:39 -0500

    commit tree f1a3ddd211bebe1251b1bdde7423741dcf2cd049

--- index.js
+++ index.js
@@ -1,1 +1,34 @@
 // Code your solution in this file!
+const returnFirstTwoDrivers = function(drivers) {
+    return drivers.slice(0, 2);
+}
+
+const returnLastTwoDrivers = function(drivers) {
+    return drivers.slice(2);
+}
+
+const selectingDrivers = [returnFirstTwoDrivers, returnLastTwoDrivers];
+
+function createFareMultiplier(integer) {
+    return function(fare) {
+        return fare * integer;
+    }
+}
+
+const fareDoubler = (function (createFareMultiplier) {
+    return createFareMultiplier * 2;
+});
+
+const fareTripler = (function(createFareMultiplier) {
+    return createFareMultiplier * 3
+});
+
+function selectDifferentDrivers(drivers,returnNewDrivers) {
+    /*return drivers.slice(0,2)*/
+    return returnNewDrivers(drivers);
+    
+}
+
+
+
+
